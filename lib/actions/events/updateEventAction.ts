d164a1d78244efc75aa0e0ab1bfb6d3b98fd6dcd
Mahendra Dani <danimahendra0904@gmail.com>
2024-02-10 10:09:04 +0530

    feat: implement update event service and action

---
+++ lib/actions/events/updateEventAction.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { updateEventById } from "@/lib/services/events/updateEventById";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export const updateEventAction = async (formData: FormData) => {
+  const resposne = await updateEventById({
+    event_name: formData.get("event_name") as string,
+    event_id: formData.get("event_id") as string,
+    event_description: formData.get("event_description") as string,
+    city: formData.get("city") as string,
+    pincode: parseInt(formData.get("pincode") as string),
+    address: formData.get("address") as string,
+    state: formData.get("state") as string,
+    country: formData.get("country") as string,
+    start_date: new Date(formData.get("start_date") as string),
+    end_date: new Date(formData.get("end_date") as string),
+    event_banner_url: (formData.get("event_banner_url") as string) || null,
+  });
+
+  // TODO : revalidate path /event/:id
+  // revalidatePath(`/events/${formData.get("event_id") as string}`)
+};
--- lib/constants/serviceMessage.ts
+++ lib/constants/serviceMessage.ts
@@ -1,4 +1,6 @@
 export const serviceMessages = {
   EVENT_FOUND: "Event successfully found by Id",
   EVENT_NOT_FOUND: "Event not found",
+  EVENT_UPDATE_SUCCESS: "Event updated successfully",
+  EVENT_UPDATE_FAILURE: "Event not updated",
 };
