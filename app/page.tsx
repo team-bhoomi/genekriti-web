09c7c5e0759640b357a930000e35e852b02c740e
Mahendra Dani <danimahendra0904@gmail.com>
2024-02-10 09:53:41 +0530

    feat: implement create event service and action

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Navbar } from "@/components/custom/navbar";
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <h1 className="text-3xl text-sky-800">Welcome to Genekriti</h1>
     </main>
   );
---
+++ lib/constants/actionMessage.ts
@@ -0,0 +1,5 @@
+export const actionMessages = {
+  EVENT_CREATED: "Event created successfully",
+  EVENT_END_DATE_BEFORE: "Event date should not be before start date",
+  EVENT_NOT_CREATED: "Event not created",
+};
