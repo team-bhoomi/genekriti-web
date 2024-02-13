97036c92ab9daff3156ffc2cf198d8a61b13bd90
Mahendra Dani <danimahendra0904@gmail.com>
2024-02-13 12:14:40 +0530

    feat: add content in dashboard page

---
+++ components/layouts/get-coins-bar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+export const GetCoinsBar = () => {
+  return (
+    <div className="flex items-center text-sm gap-2 font-medium">
+      <div className="font-semibold">Get Coins: </div>
+      <div className="flex items-center gap-2 *:px-4 *:py-1 *:rounded-md">
+        <Link
+          href={"/quiz"}
+          className="bg-[#E5AA70] hover:drop-shadow-lg"
+        >
+          Give a quiz
+        </Link>
+        <Link
+          href={"/plant-a-sap"}
+          className="bg-[#b2ec5d] hover:drop-shadow-lg"
+        >
+          Plant a Sap
+        </Link>
+        <Link
+          href={"/event"}
+          className="bg-[#87ceeb] hover:drop-shadow-lg"
+        >
+          Attend an event
+        </Link>
+      </div>
+    </div>
+  );
+};
