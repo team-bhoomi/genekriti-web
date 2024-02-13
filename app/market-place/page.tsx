0a21f3a30a3c313f9d8eaf43f816d5e040bcf033
Anushree <anushreejaiswal.srn@gmail.com>
2024-02-13 11:34:15 +0530

    created market place page

---
+++ app/market-place/page.tsx
@@ -0,0 +1,22 @@
+import { SearchBar } from "@/app/market-place/search-bar";
+import { ProductCard } from "@/components/market-place/product-card";
+
+export default function Page() {
+    return (
+        <main>
+            <SearchBar />
+            <div className="flex flex-wrap gap-10 px-4 pt-3 pb-10">
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+            </div>
+        </main>
+    );
+}
