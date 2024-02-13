4a20f8cf2799554b60efb54cb4c6dcc8b6edddb1
Anushree <anushreejaiswal.srn@gmail.com>
2024-02-13 11:35:17 +0530

    added categories data

---
+++ lib/data/categories.ts
@@ -0,0 +1,26 @@
+export const productCategories = [
+    "Accessories",
+    "Art & Craft",
+    "Bags",
+    "Cardboards",
+    "Clothes",
+    "DIY",
+    "Glass",
+    "Home Decor",
+    "Paper",
+    "Plastics",
+    "Recycling Bottles",
+];
+
+export const quizCategories = [
+    "Biodiversity",
+    "Circular Economy",
+    "Climate Change",
+    "Environmental Activism",
+    "Pollution",
+    "Renewable Energy",
+    "Sustainable Living",
+    "Sustainable Products",
+    "Sustainable Solutions",
+    "Waste Management",
+];
