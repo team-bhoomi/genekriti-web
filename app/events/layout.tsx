de155beed6b4f87582007f865bb8dfb1b87bf18f
Anushree <anushreejaiswal.srn@gmail.com>
2024-02-13 11:38:28 +0530

    updated link route

--- app/events/layout.tsx
+++ app/events/layout.tsx
@@ -13,7 +13,7 @@ export default function EventsLayout({
                 <div className="text-4xl font-semibold">Events</div>
                 {isOrg && (
                     <Link
-                        href={"/events/create-an-event"}
+                        href={"/events/create"}
                         className="group flex items-center gap-2 bg-primary/30 rounded-md text-center font-medium px-4 py-2 transition hover:bg-primary/90 hover:text-white"
                     >
                         <CalendarPlus width={18} height={18} />
