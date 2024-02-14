2d6bf72178012b61a61f244078017a2bce0a8cdb
Anushree <anushreejaiswal.srn@gmail.com>
2024-02-14 14:52:53 +0530

    created loading page

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import { Loading } from "@/components/layouts/loading";
+
+export default function Page() {
+    return (
+        <main className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center gap-5 z-30">
+            <Loading />{" "}
+        </main>
+    );
+}
