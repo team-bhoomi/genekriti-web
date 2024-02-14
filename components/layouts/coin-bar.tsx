d3f655edf9ec88c863f1e93b4848a8d233dd37e4
Mahendra Dani <danimahendra0904@gmail.com>
2024-02-14 13:44:17 +0530

    feat: balance is now shown dynamically

--- components/layouts/coin-bar.tsx
+++ components/layouts/coin-bar.tsx
@@ -1,7 +1,17 @@
+import { getUserById } from "@/lib/services/user/getUserById";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { BadgeIndianRupee } from "lucide-react";
 
-export const CoinBar = () => {
-    console.log("I AM WORKING");
+export const CoinBar = async () => {
+    const { getUser } = getKindeServerSession()
+    const user = await getUser();
+    let USER_BALANCE;
+    const { data } = await getUserById({ id: user?.id as string })
+    if (!data) {
+        USER_BALANCE = 0;
+    } else {
+        USER_BALANCE = data.balance;
+    }
     return (
         <div className="sticky top-0 right-0 bg-accent w-full h-[56px] flex items-start justify-between gap-1 pt-2 pr-2 z-20">
             <span>Welcome to Genekriti,</span>
@@ -12,7 +22,7 @@ export const CoinBar = () => {
                     fill="#ffbf00"
                     color="#5C4033"
                 />
-                <span className="text-sm font-semibold">89</span>
+                <span className="text-sm font-semibold">{USER_BALANCE}</span>
             </div>
         </div>
     );
