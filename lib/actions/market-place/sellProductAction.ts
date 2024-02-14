38486abeb6ad75cf1e13db03ad6ab85e2b37765b
Mahendra Dani <danimahendra0904@gmail.com>
2024-02-14 18:42:31 +0530

    feat: impletened sell product with UI

--- lib/actions/market-place/sellProductAction.ts
+++ lib/actions/market-place/sellProductAction.ts
@@ -1,26 +1,32 @@
+"use server";
+import { converCategoryToUpperCase } from "@/lib/constants/convertCategoryToUpperCase";
 import { sellProduct } from "@/lib/services/market-place/sellProduct";
-import { productCategory } from "@prisma/client";
+import { productCategory, questionCategory } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 export const sellProductAction = async (formData: FormData) => {
-  try {
-    const seller_id = formData.get("seller_id") as string;
-    const price = parseInt(formData.get("price") as string);
-    const name = formData.get("name") as string;
-    const description = formData.get("description") as string;
+  const seller_id = formData.get("seller_id") as string;
+  const price = parseInt(formData.get("price") as string);
+  const name = formData.get("name") as string;
+  const description = formData.get("description") as string;
+  const categories = JSON.parse(formData.get("categories") as string);
+  console.log(seller_id);
+  console.log(price);
+  console.log(name);
+  console.log(description);
+  console.log(categories);
 
-    const category1 = formData.get("category1") as productCategory;
-    const category2 = formData.get("category2") as productCategory;
-    const category3 = formData.get("category3") as productCategory;
-    const categories = [category1, category2, category3];
-    const response = await sellProduct({
-      price,
-      seller_id,
-      name,
-      description,
-      categories,
-    });
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
+  let parsedCategories: productCategory[] = [];
+  categories.forEach((category: productCategory) => {
+    let str = converCategoryToUpperCase(category);
+    parsedCategories.push(str);
+  });
+  const response = await sellProduct({
+    price,
+    seller_id,
+    name,
+    description,
+    categories: parsedCategories,
+  });
+  redirect("/market-place");
 };
