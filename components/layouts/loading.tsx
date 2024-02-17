229942cf31f7603254b6c27608f7eec3b781e352
Anushree <anushreejaiswal.srn@gmail.com>
2024-02-17 09:51:32 +0530

    updated loading component

--- components/layouts/loading.tsx
+++ components/layouts/loading.tsx
@@ -4,16 +4,21 @@ export const Loading = () => {
     const randomTip = Math.floor(Math.random() * (tips.length - 1));
     const randomTipIcon = Math.floor(Math.random() * (tipIcon.length - 1));
     return (
-        <div className="flex items-center gap-3 animate-[pulse_0.5s_ease-in-out_infinite]">
+        <div className="flex items-center gap-3">
             <div className="w-[175px] h-[175px] flex items-center">
                 <img
                     src={tipIcon[randomTipIcon].imgSrc}
                     alt={tipIcon[randomTipIcon].imgAlt}
                     className="w-full"
+                    loading="lazy"
+                    fetchPriority="high"
                 />
             </div>
-            <div className="text-xl font-semibold w-[475px]">
-                {tips[randomTip].tip}
+            <div className="flex flex-col gap-4">
+                <div className="text-xl font-semibold w-[475px]">
+                    {tips[randomTip].tip}
+                </div>
+                <div className="loader"></div>
             </div>
         </div>
     );
