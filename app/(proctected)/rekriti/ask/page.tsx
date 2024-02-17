d43d665e49835b0f7f98085697bdd9b354e272ff
Anushree <anushreejaiswal.srn@gmail.com>
2024-02-17 22:45:37 +0530

    created rekriti ask page

---
+++ app/(proctected)/rekriti/ask/page.tsx
@@ -0,0 +1,27 @@
+import { AIPrompt } from "@/components/rekriti/ai-prompt";
+import { VideoCard } from "@/components/rekriti/video-card";
+
+export default function Page() {
+    return (
+        <main className="w-full min-h-screen pr-4 flex flex-col">
+            <div className="sticky top-[56px] left-0 bg-accent flex items-center justify-between w-full text-4xl font-semibold">
+                ReKriti : Ask
+            </div>
+            <div className="flex flex-col gap-14 pt-4 px-4 pb-10">
+                <AIPrompt />
+                <div className="flex flex-col gap-3">
+                    <div className="text-2xl font-semibold">
+                        Suggested Videos :
+                    </div>
+
+                    <div className="flex flex-wrap gap-10">
+                        <VideoCard />
+                        <VideoCard />
+                        <VideoCard />
+                        <VideoCard />
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
