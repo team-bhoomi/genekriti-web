0d32dd7aa9707d87f2de13fb9fab62cd5cf152cc
Mahendra Dani <116940083+MahendraDani@users.noreply.github.com>
2024-02-17 17:37:43 +0530

    Revert "feat: handled edge cases for profile page when user has no data"

--- components/profile/product-purchase-history-card.tsx
+++ components/profile/product-purchase-history-card.tsx
@@ -3,7 +3,6 @@ import { Card, CardContent } from "../ui/card";
 import dayjs from "dayjs";
 import { Badge } from "../ui/badge";
 import { getProductyId } from "@/lib/services/market-place/getProductById";
-import { BadgeIndianRupee } from "lucide-react";
 
 export const ProductPurchaseHistoryCard = async ({ product }: { product: any }) => {
   const data = await getProductyId({ product_id: product.product_id, is_sold: true })
@@ -21,18 +20,7 @@ export const ProductPurchaseHistoryCard = async ({ product }: { product: any }) 
               : <Badge variant={"secondary"}>Available</Badge>}
           </div>
         </CardContent>
-        <div className="font-medium p-4">
-          <div className="flex justify-start items-center gap-1">
-            <BadgeIndianRupee
-              width={20}
-              height={20}
-              fill="#ffbf00"
-              color="#5C4033"
-            />{" "}
-            <span>{data?.is_sold ? <span className="text-red-600"> - </span> : <span className="text-green-600">+</span>}</span>
-            <span>{data?.price}</span>
-          </div>
-        </div>
+        <div className="font-medium p-4">Price: Rs {product.price}</div>
       </div>
       <div className="w-full p-4 pt-0">Date: {dayjs(product.created_at).format("DD/MM/YYYY")}</div>
     </Card>
